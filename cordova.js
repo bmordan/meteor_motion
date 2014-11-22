e2c5d892dc11fe1a56a49436baa656641f916a88
Bernard Mordan <bernardmordan@gmail.com>
2014-11-22 09:20:23 +0000

    working example of outputting DeviceMotionEvent

--- cordova.js
+++ cordova.js
@@ -1,12 +1,29 @@
 if (Meteor.isClient) {
 
-  Template.area.rendered = function(){
-    var el = this.find('section')
+  var stream;
+
+  Template.press.rendered = function(){
+    el = this.find('section')
     Hammer(el).on('press', function(e){
+      stream = true
       $('samp').text(e.type)
-      console.log('press')
     })
-  }
+    Hammer(el).on('hammer.input', function(e){
+      if(e.isFirst === false){
+        stream = false
+        $('cite').text('motion')
+        $('samp').text('Press & Hold')
+      }
+    })
+  };
+
+  if(window.DeviceMotionEvent){
+    window.addEventListener('devicemotion', function(m){
+      var x  = m.acceleration.x
+      if(stream) $('cite').text(x.toPrecision(4))
+    })
+  };
+
 }
 
 if (Meteor.isServer) {
